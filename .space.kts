cb3b1fda0e89ac02d4e3dd21107a9428c55bfa8e
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-01-17 09:04:01 +0100

    Add space jobs

---
+++ .space.kts
@@ -0,0 +1,22 @@
+job("HyperstyleUtilities / Release / Docker") {
+    startOn {
+        gitPush {
+            enabled = false
+        }
+    }
+
+    val version = "0.0.1"
+    val type = "hyperstyle-utilities"
+
+    kaniko {
+        build {
+            context = "."
+            dockerfile = "Dockerfile"
+            target = type
+            args["VERSION"] = version
+        }
+        push("registry.jetbrains.team/p/hyperstyle-utilities/docker/${type}") {
+            tags(version)
+        }
+    }
+}
