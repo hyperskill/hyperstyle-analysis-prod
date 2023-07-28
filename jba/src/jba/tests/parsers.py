47478d71052b1c0a9665bc691df410c96a81de26
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-07-28 14:43:00 +0400

    refactor: move parsers to a separate file

---
+++ jba/src/jba/tests/parsers.py
@@ -0,0 +1,110 @@
+import re
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from pathlib import Path
+from typing import List
+
+from jba.models.edu_logs import TestData, TestDataField, ExceptionData
+
+EXCEPTION_REGEXP = re.compile(r'^e: (.*): \((\d+), (\d+)\): (.*)$')
+CLASS_NAME_REGEXP = re.compile(r'^Class (.*)$')
+
+
+def parse_gradle_test_logs(test_logs_path: Path) -> List[TestData]:
+    """
+    Parse gradle test logs into list of `TestData`.
+
+    :param test_logs_path: Path to an HTML file with gradle test logs.
+    :return: List of `TestData`.
+    """
+    with open(test_logs_path) as file:
+        logs = file.read()
+
+    soup = BeautifulSoup(logs, 'html.parser')
+
+    # Find a tab with failed tests
+    failed_test_tab = soup.find('ul', {'class': 'tabLinks'}).find(
+        lambda tag: tag.name == 'a' and 'Failed tests' in tag.text.strip()
+    )
+
+    failed_tests = {}
+    if failed_test_tab:
+        failed_test_tab_id = failed_test_tab['href'].removeprefix('#')
+
+        for failed_test in soup.find('div', {'id': failed_test_tab_id}).find_all('div', {'class': 'test'}):
+            method_name = failed_test.find('a')['name']
+            test = failed_test.find('h3', {'class': 'failures'}).text.strip()
+            logs = failed_test.find('span', {'class': 'code'}).text.strip()
+
+            delimiter_index = logs.find(':')
+            error_class = logs[:delimiter_index].strip()
+            message = logs[delimiter_index + 1 : logs.find('\tat ')].strip()
+
+            failed_tests[(test, method_name)] = {
+                TestDataField.ERROR_CLASS.value: error_class,
+                TestDataField.MESSAGE.value: message,
+            }
+
+    # Find id of a tab with all tests
+    test_table_tab_id = (
+        soup.find('ul', {'class': 'tabLinks'})
+        .find(lambda tag: tag.name == 'a' and "Tests" in tag.text)['href']
+        .removeprefix('#')
+    )
+
+    test_table = pd.read_html(str(soup.find('div', {'id': test_table_tab_id})))[0]
+    test_table.columns = map(lambda name: name.lower().replace(' ', '_'), test_table.columns)
+
+    class_name_block = soup.find(lambda tag: tag.name == 'h1' and re.match(CLASS_NAME_REGEXP, tag.text.strip()))
+    class_name = re.match(CLASS_NAME_REGEXP, class_name_block.text.strip()).group(1)
+
+    tests = []
+    for row in test_table.itertuples():
+        test = getattr(row, TestDataField.TEST.value)
+        method_name = getattr(row, TestDataField.METHOD_NAME.value, test)
+
+        tests.append(
+            TestData(
+                class_name=class_name,
+                test=test,
+                method_name=method_name,
+                duration=getattr(row, TestDataField.DURATION.value),
+                result=getattr(row, TestDataField.RESULT.value),
+                error_class=(failed_tests.get((test, method_name), {}).get(TestDataField.ERROR_CLASS.value)),
+                message=failed_tests.get((test, method_name), {}).get(TestDataField.MESSAGE.value),
+            )
+        )
+
+    return tests
+
+
+def parse_gradle_stderr_logs(stderr_logs_path: Path, task_path: str) -> List[ExceptionData]:
+    """
+    Parse gradle stderr logs into list of `ExceptionData`.
+
+    :param stderr_logs_path: Path to a text file with gradle stderr logs.
+    :param task_path: Relative path from a course root to a task folder.
+    :return: List of `ExceptionData`.
+    """
+
+    exceptions = []
+    with open(stderr_logs_path) as file:
+        for line in file:
+            match = re.match(EXCEPTION_REGEXP, line)
+            if not match:
+                continue
+
+            full_file_path = match.group(1)
+            relative_file_path = full_file_path[full_file_path.find(task_path) + len(task_path) + 1 :]
+
+            exceptions.append(
+                ExceptionData(
+                    path=relative_file_path,
+                    line_number=int(match.group(2)),
+                    column_number=int(match.group(3)),
+                    message=match.group(4),
+                ),
+            )
+
+    return exceptions
--- jba/src/jba/tests/tests_parser.py
+++ jba/src/jba/tests/tests_parser.py
@@ -1,71 +1,37 @@
-from enum import Enum, unique
-
+# TODO: check namings
 import argparse
 import json
 import logging
 import os
 import pandas as pd
-import re
-from bs4 import BeautifulSoup
-from dataclasses import dataclass, asdict
+from dataclasses import asdict
 from pathlib import Path
 from typing import Optional, List, Tuple
 
 from core.utils.df_utils import read_df, write_df
-from core.utils.file.file_utils import get_output_path, get_name_from_path
+from core.utils.file.file_utils import get_output_path
 from jba.models.edu_columns import EduColumnName
+from jba.models.edu_logs import ExceptionData, TestData
+from jba.tests.parsers import parse_gradle_test_logs, parse_gradle_stderr_logs
 from jba.tests.tests_runner import GRADLE_STDERR_LOGS_FILE, TEST_LOGS_FOLDER_NAME
 
 logger = logging.getLogger(__name__)
 
-EXCEPTION_REGEXP = re.compile(r'^e: (.*): \((\d+), (\d+)\): (.*)$')
 
-
-@unique
-class TestDataField(Enum):
-    CLASS_NAME = 'class_name'
-    TEST = 'test'
-    METHOD_NAME = 'method_name'
-    DURATION = 'duration'
-    RESULT = 'result'
-    ERROR_CLASS = 'error_class'
-    MESSAGE = 'message'
-
-
-@dataclass
-class TestData:
-    class_name: str
-    test: str
-    method_name: str
-    duration: str
-    result: str
-
-    error_class: Optional[str] = None
-    message: Optional[str] = None
-
-
-@dataclass
-class ExceptionData:
-    path: str
-    line_number: int
-    column_number: int
-    message: str
-
-
-def parse_gradle_logs(submission: pd.Series, gradle_logs_path: Path) -> Tuple[Optional[str], Optional[str]]:
+def parse_gradle_logs(submission: pd.Series, gradle_logs_folder: Path) -> Tuple[Optional[str], Optional[str]]:
     """
-    Parse Gradle stderr and test logs.
+    Parse gradle stderr and test logs.
 
     :param submission: User's submission data.
-    :param gradle_logs_path: Path to a folder with gradle logs.
+    :param gradle_logs_folder: Path to a folder with gradle logs.
     :return: Parsed stderr and test logs as json strings.
     """
     submission_id = submission[EduColumnName.ID.value]
 
     logger.info(f'Parsing submission#{submission_id}')
 
-    submission_gradle_logs_path = gradle_logs_path / str(submission_id)
-    if not submission_gradle_logs_path.exists():
+    submission_gradle_logs_folder = gradle_logs_folder / str(submission_id)
+    if not submission_gradle_logs_folder.exists():
         logger.warning("Submission Gradle logs are missing")
         return None, None
 
@@ -75,23 +41,23 @@ def parse_gradle_logs(submission: pd.Series, gradle_logs_path: Path) -> Tuple[Op
 
     task_path = Path('' if section_name is None else section_name) / lesson_name / task_name
 
-    parsed_stderr_logs = _parse_stderr_logs(str(task_path), submission_gradle_logs_path)
-    if parsed_stderr_logs:
+    parsed_stderr_logs = _parse_stderr_logs(submission_gradle_logs_folder, str(task_path))
+    if parsed_stderr_logs is not None:
         parsed_stderr_logs = json.dumps([asdict(exception) for exception in parsed_stderr_logs])
 
-    parsed_test_logs = _parse_test_logs(submission_gradle_logs_path)
-    if parsed_test_logs:
+    parsed_test_logs = _parse_test_logs(submission_gradle_logs_folder)
+    if parsed_test_logs is not None:
         parsed_test_logs = json.dumps([asdict(test) for test in parsed_test_logs])
 
     return parsed_stderr_logs, parsed_test_logs
 
 
-def _parse_stderr_logs(task_path: str, submission_gradle_logs_path: Path) -> Optional[List[ExceptionData]]:
+def _parse_stderr_logs(submission_gradle_logs_path: Path, task_path: str) -> Optional[List[ExceptionData]]:
     """
-    Parse Gradle stderr logs into list of `ExceptionData`.
+    Parse submission gradle stderr logs into list of `ExceptionData`.
 
+    :param submission_gradle_logs_path: Path to a folder with submission gradle logs.
     :param task_path: Relative path from a course root to a task.
-    :param submission_gradle_logs_path: Path to a file with gradle stderr logs.
     :return: List of `ExceptionData`.
     """
     logger.info('Parsing stderr logs')
@@ -101,25 +67,7 @@ def _parse_stderr_logs(task_path: str, submission_gradle_logs_path: Path) -> Opt
         logger.info('Stderr logs file does not exist')
         return None
 
-    exceptions = []
-    with open(stderr_logs_file) as file:
-        for line in file:
-            match = re.match(EXCEPTION_REGEXP, line)
-            if not match:
-                continue
-
-            full_file_path = match.group(1)
-            relative_file_path = full_file_path[full_file_path.find(task_path) + len(task_path) + 1:]
-
-            exceptions.append(
-                ExceptionData(
-                    path=relative_file_path,
-                    line_number=int(match.group(2)),
-                    column_number=int(match.group(3)),
-                    message=match.group(4),
-                ),
-            )
-
+    exceptions = parse_gradle_stderr_logs(stderr_logs_file, task_path)
     if not exceptions:
         logger.info('There are no exceptions in the stderr logs file')
 
@@ -128,77 +76,23 @@ def _parse_stderr_logs(task_path: str, submission_gradle_logs_path: Path) -> Opt
 
 def _parse_test_logs(submission_gradle_logs_path: Path) -> Optional[List[TestData]]:
     """
-    Parse Gradle test logs into list of `TestData`.
+    Parse submission gradle test logs into list of `TestData`.
 
-    :param submission_gradle_logs_path: Path to a file with gradle stderr logs.
+    :param submission_gradle_logs_path: Path to a folder with submission gradle logs.
     :return: List of `TestData`.
     """
     logger.info('Parsing test logs')
 
-    test_logs_path = submission_gradle_logs_path / TEST_LOGS_FOLDER_NAME / 'classes'
-    if not test_logs_path.exists():
+    test_gradle_logs_path = submission_gradle_logs_path / TEST_LOGS_FOLDER_NAME / 'classes'
+    if not test_gradle_logs_path.exists():
         logger.info('Test logs file does not exist')
         return None
 
-    tests = []
-    for test_class_file in os.listdir(test_logs_path):
-        test_class_logs_path = test_logs_path / test_class_file
-
-        with open(test_class_logs_path) as file:
-            logs = file.read()
-
-        soup = BeautifulSoup(logs, 'html.parser')
-
-        # Find a tab with failed tests
-        failed_test_tab = soup.find('ul', {'class': 'tabLinks'}).find(
-            lambda tag: tag.name == 'a' and 'Failed tests' in tag.text.strip()
-        )
-
-        failed_tests = {}
-        if failed_test_tab:
-            failed_test_tab_id = failed_test_tab['href'].removeprefix('#')
-
-            for failed_test in soup.find('div', {'id': failed_test_tab_id}).find_all('div', {'class': 'test'}):
-                test = failed_test.find('h3', {'class': 'failures'}).text.strip()
-                logs = failed_test.find('span', {'class': 'code'}).text.strip()
-
-                delimiter_index = logs.find(':')
-                error_class = logs[:delimiter_index].strip()
-                message = logs[delimiter_index + 1 : logs.find('\tat ')].strip()
-
-                failed_tests[test] = {
-                    TestDataField.ERROR_CLASS.value: error_class,
-                    TestDataField.MESSAGE.value: message,
-                }
-
-        # Find id of a tab with all tests
-        test_table_tab_id = (
-            soup.find('ul', {'class': 'tabLinks'})
-            .find(lambda tag: tag.name == 'a' and "Tests" in tag.text.strip())['href']
-            .removeprefix('#')
-        )
-
-        test_table = pd.read_html(str(soup.find('div', {'id': test_table_tab_id})))[0]
-        test_table.columns = map(lambda name: name.lower().replace(' ', '_'), test_table.columns)
-
-        tests.extend(
-            test_table.apply(
-                lambda row: TestData(
-                    class_name=get_name_from_path(test_class_file, with_extension=False),
-                    test=row[TestDataField.TEST.value],
-                    method_name=row.get(TestDataField.METHOD_NAME.value, row[TestDataField.TEST.value]),
-                    duration=row[TestDataField.DURATION.value],
-                    result=row[TestDataField.RESULT.value],
-                    error_class=(
-                        failed_tests.get(row[TestDataField.TEST.value], {}).get(TestDataField.ERROR_CLASS.value)
-                    ),
-                    message=failed_tests.get(row[TestDataField.TEST.value], {}).get(TestDataField.MESSAGE.value),
-                ),
-                axis=1,
-            ),
-        )
-
-    return tests
+    return [
+        test_data
+        for test_class_file in os.listdir(test_gradle_logs_path)
+        for test_data in parse_gradle_test_logs(test_gradle_logs_path / test_class_file)
+    ]
 
 
 def configure_parser(parser: argparse.ArgumentParser) -> None:
@@ -236,7 +130,7 @@ def main():
         lambda row: parse_gradle_logs(row, args.gradle_logs_path),
         axis=1,
         result_type='expand',
-    ).rename(columns={0: 'exceptions', 1: 'tests'})
+    ).rename(columns={0: EduColumnName.EXCEPTIONS.value, 1: EduColumnName.TESTS.value})
 
     submissions_with_parsed_logs = pd.concat([submissions, parsed_logs], axis=1)
     write_df(submissions_with_parsed_logs, get_output_path(args.submissions_path, '-with_parsed_logs'))
