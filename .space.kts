cd6dce8bd1249a68a39619d103b894380f00c3aa
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-08-10 18:06:46 +0400

    feat: add the release Docker job

--- .space.kts
+++ .space.kts
@@ -1,3 +1,5 @@
+import java.io.File
+
 job("Release base Docker") {
     startOn {
         gitPush {
@@ -11,6 +13,32 @@ job("Release base Docker") {
     kaniko {
         build {
             dockerfile = "./Dockerfile.base"
+        }
+
+        push("registry.jetbrains.team/p/code-quality-for-online-learning-platforms/hyperstyle-analysis-prod/${type}") {
+            tags(version)
+        }
+    }
+}
+
+
+job("Release Docker") {
+    startOn {
+        gitPush {
+            enabled = false
+        }
+    }
+
+    val versionRegex = Regex("^version = \"(.*)\"\$")
+    val version = File("pyproject.toml").useLines { lines ->
+        lines.firstNotNullOf { versionRegex.find(it) }.groupValues.last()
+    }
+
+    val type = "hyperstyle-analysis-prod"
+
+    kaniko {
+        build {
+            dockerfile = "./Dockerfile"
         }
 
         push("registry.jetbrains.team/p/code-quality-for-online-learning-platforms/hyperstyle-analysis-prod/${type}") {
