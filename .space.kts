8fb79a8124c63b750fce545f822e67d3a9ab7ebe
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-08-10 16:11:37 +0400

    fix: add the release Dockerfile.base job

--- .space.kts
+++ .space.kts
@@ -1,21 +1,20 @@
-job("HyperstyleUtilities / Release / Docker") {
+job("Hyperstyle Analysis Prod | Release base Docker") {
     startOn {
         gitPush {
             enabled = false
         }
     }
 
-    val version = "0.0.1"
-    val type = "hyperstyle-utilities"
+    val version = "py3.9.17-java17.0.8.7"
+    val type = "hyperstyle-analysis-prod-base"
 
     kaniko {
         build {
             context = "."
-            dockerfile = "Dockerfile"
-            target = "paddle-py-3-9"
-            args["VERSION"] = version
+            dockerfile = "Dockerfile.base"
         }
-        push("registry.jetbrains.team/p/code-quality-for-online-learning-platforms/hyperstyle-utilities/${type}") {
+
+        push("registry.jetbrains.team/p/code-quality-for-online-learning-platforms/hyperstyle-analysis-prod/${type}") {
             tags(version)
         }
     }
