ad97cb602ddfcb83818ffd5461cb4900d917c62a
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-08-18 18:09:28 +0400

    fix: fix the bug when tasks with the same name were merged together

--- jba/tests/plots/test_task_duplicates.py
+++ jba/tests/plots/test_task_duplicates.py
@@ -1,10 +1,19 @@
-from typing import List
+from typing import List, Optional
 
 import pandas as pd
 import pytest
+from pandas._testing import assert_series_equal
 
+from core.src.model.column_name import SubmissionColumns
 from jba.src.models.edu_columns import EduColumnName
-from jba.src.plots.task_duplicates import _count_duplicates_submissions
+from jba.src.plots.task_duplicates import (
+    _count_duplicates_submissions,
+    _compute_stats,
+    MIN_STATS_COLUMN,
+    MAX_STATS_COLUMN,
+    MEDIAN_STATS_COLUMN,
+    MEAN_STATS_COLUMNS,
+)
 
 COUNT_DUPLICATES_TEST_DATA = [
     (pd.DataFrame(), []),
@@ -22,3 +31,69 @@ COUNT_DUPLICATES_TEST_DATA = [
 def test_count_duplicates(group: pd.DataFrame, expected_duplicates: List[int]):
     group.name = 0
     assert _count_duplicates_submissions(group) == expected_duplicates
+
+
+COURSE_DATA = pd.DataFrame(
+    [
+        [1, 6, 'A'],
+        [1, 6, 'B'],
+        [2, 1, 'A'],
+        [2, 2, 'A'],
+        [2, 2, 'B'],
+        [2, 2, 'C'],
+        [3, 3, 'A'],
+        [3, 3, 'B'],
+        [3, 3, 'B'],
+        [3, 3, 'B'],
+        [3, 3, 'A'],
+        [3, 3, 'C'],
+        [3, 4, 'A'],
+        [3, 4, 'B'],
+        [3, 4, 'A'],
+        [3, 5, 'A'],
+        [3, 5, 'A'],
+        [3, 5, 'A'],
+        [3, 5, 'A'],
+        [3, 5, 'A'],
+        [3, 5, 'B'],
+        [4, 7, 'A'],
+        [4, 7, 'A'],
+        [4, 7, 'B'],
+        [4, 8, 'A'],
+        [4, 8, 'A'],
+        [4, 8, 'A'],
+        [4, 8, 'A'],
+        [4, 8, 'B'],
+        [4, 8, 'A'],
+        [4, 8, 'A'],
+        [4, 9, 'A'],
+        [4, 9, 'A'],
+        [4, 9, 'B'],
+        [4, 9, 'B'],
+        [4, 9, 'C'],
+        [4, 9, 'C'],
+        [4, 9, 'D'],
+        [4, 9, 'D'],
+        [4, 9, 'E'],
+        [4, 9, 'E'],
+    ],
+    columns=[EduColumnName.TASK_ID.value, SubmissionColumns.GROUP.value, EduColumnName.CODE_SNIPPETS.value],
+)
+
+
+COMPUTE_STATA_TEST_DATA = [
+    (0, None),
+    (1, pd.Series({MIN_STATS_COLUMN: 0.0, MAX_STATS_COLUMN: 0.0, MEAN_STATS_COLUMNS: 0.0, MEDIAN_STATS_COLUMN: 0.0})),
+    (2, pd.Series({MIN_STATS_COLUMN: 0.0, MAX_STATS_COLUMN: 0.0, MEAN_STATS_COLUMNS: 0.0, MEDIAN_STATS_COLUMN: 0.0})),
+    (3, pd.Series({MIN_STATS_COLUMN: 0.0, MAX_STATS_COLUMN: 4.0, MEAN_STATS_COLUMNS: 2.0, MEDIAN_STATS_COLUMN: 2.0})),
+    (4, pd.Series({MIN_STATS_COLUMN: 1.0, MAX_STATS_COLUMN: 3.0, MEAN_STATS_COLUMNS: 5/3, MEDIAN_STATS_COLUMN: 1.0})),
+]
+
+
+@pytest.mark.parametrize(('task_id', 'expected_stats'), COMPUTE_STATA_TEST_DATA)
+def test_compute_stats(task_id: int, expected_stats: Optional[pd.Series]):
+    actual_stats = _compute_stats(task_id, COURSE_DATA)
+    if expected_stats is None:
+        assert actual_stats is None
+    else:
+        assert_series_equal(actual_stats, expected_stats)
