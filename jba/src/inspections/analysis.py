8fa7bfe016a561e00b738ede8b1f2839a2161382
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-10-26 15:28:41 +0400

    feat: added the ability to select a file

--- jba/src/inspections/analysis.py
+++ jba/src/inspections/analysis.py
@@ -1,42 +1,59 @@
 from collections import Counter
+from dataclasses import dataclass
 from itertools import islice
-from typing import Set, List, Tuple
+from typing import Set, List, Optional, Dict
 
 import pandas as pd
 
 from core.src.model.column_name import SubmissionColumns
 from core.src.model.quality.issue.hyperstyle_issue import HyperstyleIssue
 from jba.src.models.edu_columns import EduColumnName
+from jba.src.visualization.common import get_edu_name_columns
 
 
-def find_unique_inspections(group: pd.DataFrame) -> Set[str]:
-    return set(
-        group[EduColumnName.INSPECTIONS.value]
-        .apply(lambda inspections: {inspection.code for inspection in inspections})
-        .explode()
-        .unique()
+def find_unique_inspections(group: pd.DataFrame, file: Optional[str]) -> Set[str]:
+    group_inspections = group[EduColumnName.INSPECTIONS.value].apply(
+        lambda inspections: {
+            inspection.code
+            for file_path, file_inspections in inspections.items()
+            for inspection in file_inspections
+            if file is None or file_path == file
+        }
     )
 
+    return set(group_inspections.explode().unique())
 
-def find_fixed_unique_inspections(group: pd.DataFrame) -> Set[str]:
-    unique_inspections = find_unique_inspections(group)
+
+def find_fixed_unique_inspections(group: pd.DataFrame, file: Optional[str]) -> Set[str]:
+    unique_inspections = find_unique_inspections(group, file)
 
     last_attempt = group.loc[group[SubmissionColumns.ATTEMPT.value].idxmax()].squeeze()
-    last_attempt_inspections = {inspection.code for inspection in last_attempt[EduColumnName.INSPECTIONS.value]}
+    last_attempt_inspections = {
+        inspection.code
+        for file_path, file_inspections in last_attempt[EduColumnName.INSPECTIONS.value].items()
+        for inspection in file_inspections
+        if file is None or file_path == file
+    }
 
     return unique_inspections - last_attempt_inspections
 
 
-def find_not_fixed_unique_inspections(group: pd.DataFrame) -> Set[str]:
-    not_fixed_unique_inspections = find_unique_inspections(group)
+def find_not_fixed_unique_inspections(group: pd.DataFrame, file: Optional[str]) -> Set[str]:
+    not_fixed_unique_inspections = find_unique_inspections(group, file)
 
     for previous_row, current_row in zip(group.itertuples(index=False), islice(group.itertuples(index=False), 1, None)):
         previous_number_of_inspections = Counter(
-            inspection.code for inspection in getattr(previous_row, EduColumnName.INSPECTIONS.value)
+            inspection.code
+            for file_path, file_inspections in getattr(previous_row, EduColumnName.INSPECTIONS.value).items()
+            for inspection in file_inspections
+            if file is None or file_path == file
         )
 
         current_number_of_inspections = Counter(
-            inspection.code for inspection in getattr(current_row, EduColumnName.INSPECTIONS.value)
+            inspection.code
+            for file_path, file_inspections in getattr(current_row, EduColumnName.INSPECTIONS.value).items()
+            for inspection in file_inspections
+            if file is None or file_path == file
         )
 
         for inspection in previous_number_of_inspections.keys() | current_number_of_inspections.keys():
@@ -48,23 +65,33 @@ def find_not_fixed_unique_inspections(group: pd.DataFrame) -> Set[str]:
 
 def get_unique_inspections_stats(
     df: pd.DataFrame,
+    file: Optional[str],
     inspections_to_ignore: List[str],
     normalize: bool = True,
 ) -> pd.DataFrame:
     unique_inspections = (
-        df.groupby(SubmissionColumns.GROUP.value).apply(find_unique_inspections).explode().value_counts()
+        df.groupby(SubmissionColumns.GROUP.value)
+        .apply(lambda group: find_unique_inspections(group, file))
+        .explode()
+        .value_counts()
     )
 
     unique_inspections.name = 'Total'
 
     fixed_unique_inspections = (
-        df.groupby(SubmissionColumns.GROUP.value).apply(find_fixed_unique_inspections).explode().value_counts()
+        df.groupby(SubmissionColumns.GROUP.value)
+        .apply(lambda group: find_fixed_unique_inspections(group, file))
+        .explode()
+        .value_counts()
     )
 
     fixed_unique_inspections.name = 'Fixed'
 
     not_fixed_unique_inspections = (
-        df.groupby(SubmissionColumns.GROUP.value).apply(find_not_fixed_unique_inspections).explode().value_counts()
+        df.groupby(SubmissionColumns.GROUP.value)
+        .apply(lambda group: find_not_fixed_unique_inspections(group, file))
+        .explode()
+        .value_counts()
     )
 
     not_fixed_unique_inspections.name = 'Not fixed'
@@ -89,28 +116,52 @@ def get_unique_inspections_stats(
     return stats
 
 
-def get_inspection_fixing_examples(  # noqa: WPS234
+@dataclass
+class FixingExample:
+    task_name: str
+    file_path: str
+    before_issues: List[HyperstyleIssue]
+    before_code: str
+    after_issues: List[HyperstyleIssue]
+    after_code: str
+
+
+def _find_code_snippet(code_snippets: List[Dict[str, str]], file: str) -> str:
+    return next(filter(lambda snippet: file == snippet['name'], code_snippets))['text']
+
+
+def get_inspection_fixing_examples(
     group: pd.DataFrame,
     inspection_name: str,
-) -> List[Tuple[List[HyperstyleIssue], str, List[HyperstyleIssue], str]]:
+    file: Optional[str],
+) -> List[FixingExample]:
+    edu_name_columns = get_edu_name_columns(group)
+    task_name = "/".join(group[edu_name_columns].values[0])
+
     examples = []
 
     for previous_row, current_row in zip(group.itertuples(index=False), islice(group.itertuples(index=False), 1, None)):
-        previous_issues = [
-            inspection
-            for inspection in getattr(previous_row, EduColumnName.INSPECTIONS.value)
-            if inspection.code == inspection_name
-        ]
+        previous_issues_per_file = {
+            file_path: [inspection for inspection in file_inspections if inspection.code == inspection_name]
+            for file_path, file_inspections in getattr(previous_row, EduColumnName.INSPECTIONS.value).items()
+            if file is None or file_path == file
+        }
 
-        current_issues = [
-            inspection
-            for inspection in getattr(current_row, EduColumnName.INSPECTIONS.value)
-            if inspection.code == inspection_name
-        ]
+        current_issues_per_file = {
+            file_path: [inspection for inspection in file_inspections if inspection.code == inspection_name]
+            for file_path, file_inspections in getattr(current_row, EduColumnName.INSPECTIONS.value).items()
+            if file is None or file_path == file
+        }
 
-        if len(current_issues) < len(previous_issues):
-            previous_code = getattr(previous_row, EduColumnName.CODE_SNIPPETS.value)
-            current_code = getattr(current_row, EduColumnName.CODE_SNIPPETS.value)
-            examples.append((previous_issues, previous_code, current_issues, current_code))
+        for file_path in previous_issues_per_file.keys():
+            previous_issues = previous_issues_per_file[file_path]
+            current_issues = current_issues_per_file[file_path]
+
+            if len(current_issues) < len(previous_issues):
+                previous_code = _find_code_snippet(getattr(previous_row, EduColumnName.CODE_SNIPPETS.value), file_path)
+                current_code = _find_code_snippet(getattr(current_row, EduColumnName.CODE_SNIPPETS.value), file_path)
+                examples.append(
+                    FixingExample(task_name, file_path, previous_issues, previous_code, current_issues, current_code)
+                )
 
     return examples
