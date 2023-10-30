241d9288026bf67270262015b80ebb9cad8a8a6c
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-10-30 18:26:00 +0400

    feat: add basic stats

--- jba/src/visualization/pages/1_General_stats.py
+++ jba/src/visualization/pages/1_General_stats.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
+from core.src.model.column_name import SubmissionColumns
 from core.src.utils.df_utils import read_df
+from jba.src.models.edu_columns import EduColumnName
 from jba.src.plots.task_attempt import plot_task_attempts
 from jba.src.plots.task_duplicates import plot_task_duplicates
 from jba.src.plots.task_solving import plot_task_solving
@@ -14,9 +16,17 @@ def main():
     submissions = read_df(st.session_state.submissions_path)
     course_structure = read_df(st.session_state.course_structure_path)
 
-    # TODO: add basic stats
-    # TODO: do we really need a course structure in this visualization?
-    #       Maybe we could use structure from submissions
+    st.header('Basic stats')
+
+    left, middle, right = st.columns(3)
+    with left:
+        st.metric('Number of submissions:', len(submissions))
+
+    with middle:
+        st.metric('Number of users:', submissions[EduColumnName.USER_ID.value].nunique())
+
+    with right:
+        st.metric('Number of solution groups:', submissions[SubmissionColumns.GROUP.value].nunique())
 
     st.header('Task attempts')
     fig = plot_task_attempts(submissions, course_structure, st.session_state.course_name)
