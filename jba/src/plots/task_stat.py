6aaaad7637a2589eabfda0a311d8690bfbd36e69
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-11-30 15:16:25 +0400

    fix: fix a bug in the `calculate_tasks_stat` function that causes stats to calculate incorrectly

--- jba/src/plots/task_stat.py
+++ jba/src/plots/task_stat.py
@@ -4,16 +4,14 @@ from typing import Dict
 import pandas as pd
 
 from core.src.model.column_name import SubmissionColumns
-from core.src.utils.df_utils import filter_df_by_single_value
 from jba.src.models.edu_columns import EduColumnName, EduTaskStatus
 
 
 @dataclass(frozen=True)
 class TaskStat:
-    started: int
-    finished: int
-    finished_correct: int
-    unfinished: int
+    total_groups: int
+    correct_groups: int
+    wrong_groups: int
 
     median_attempts: int
     min_attempts: int
@@ -21,24 +19,31 @@ class TaskStat:
 
 
 def calculate_tasks_stat(course_data_df: pd.DataFrame) -> Dict[int, TaskStat]:
-    tasks = course_data_df[EduColumnName.TASK_ID.value].unique()
     stat = {}
-    for task_id in tasks:
-        task_data = filter_df_by_single_value(course_data_df, EduColumnName.TASK_ID.value, task_id)
-        started_df = filter_df_by_single_value(task_data, SubmissionColumns.ATTEMPT.value, 1)
-        started = started_df.shape[0]
-        finished_df = task_data.loc[
-            (task_data[SubmissionColumns.ATTEMPT.value] == task_data[SubmissionColumns.TOTAL_ATTEMPTS.value])]
-        finished = finished_df.shape[0]
-        finished_correct = filter_df_by_single_value(finished_df, EduColumnName.STATUS.value,
-                                                     EduTaskStatus.CORRECT.value).shape[0]
-        unfinished = filter_df_by_single_value(finished_df, EduColumnName.STATUS.value,
-                                               EduTaskStatus.WRONG.value).shape[0]
+    for task_id, task_data in course_data_df.groupby(EduColumnName.TASK_ID.value):
+        total_groups = task_data[SubmissionColumns.GROUP.value].nunique()
 
-        median_attempts = task_data[SubmissionColumns.ATTEMPT.value].median()
-        min_attempts = task_data[SubmissionColumns.ATTEMPT.value].min()
-        max_attempts = task_data[SubmissionColumns.ATTEMPT.value].max()
+        correct_groups = (
+            task_data.groupby(SubmissionColumns.GROUP.value)
+            .apply(lambda group: EduTaskStatus.CORRECT.value in group[EduColumnName.STATUS.value].unique())
+            .sum()
+        )
 
-        stat[task_id] = TaskStat(started, finished, finished_correct, unfinished,
-                                 median_attempts, min_attempts, max_attempts)
+        wrong_groups = total_groups - correct_groups
+
+        total_attempts_by_group = task_data.groupby(SubmissionColumns.GROUP.value).apply(len)
+
+        median_attempts = total_attempts_by_group.median()
+        min_attempts = total_attempts_by_group.min()
+        max_attempts = total_attempts_by_group.max()
+
+        stat[task_id] = TaskStat(
+            total_groups,
+            correct_groups,
+            wrong_groups,
+            median_attempts,
+            min_attempts,
+            max_attempts,
+        )
+
     return stat
