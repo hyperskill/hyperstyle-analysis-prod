54abf0880bb055bdb3406852889accd77dd8567a
mikrise2 <mikrise2@mail.ru>
2024-01-30 13:37:41 +0100

    yaml_as_dict changed to parse_yaml

--- jba/tests/processing/test_tasktracker_content_collector.py
+++ jba/tests/processing/test_tasktracker_content_collector.py
@@ -4,14 +4,13 @@ from pathlib import Path
 
 import pytest
 
+from core.src.utils.file.yaml_utils import parse_yaml
 from core.src.utils.subprocess_runner import run_in_subprocess
 from jba.src import MAIN_FOLDER
 
 from jba.tests.processing import PROCESSING_FOLDER
 
 import tempfile
-
-import yaml
 
 from deepdiff import DeepDiff  # noqa: SC200
 
@@ -23,16 +22,6 @@ def test_incorrect_arguments():
 
     assert stdout == ''
     assert 'error: the following arguments are required' in stderr
-
-
-def yaml_as_dict(my_file):
-    result = {}
-    with open(my_file, 'r') as file_path:
-        docs = yaml.safe_load_all(file_path)
-        for doc in docs:
-            for key, value in doc.items():
-                result[key] = value
-    return result
 
 
 TEST_DATA = ['course_with_section', 'course_without_section', 'course_with_framework']
@@ -52,5 +41,5 @@ def test_course(course_name):
         file = Path(temp_directory) / 'task_content_default.yaml'
         assert file.exists()
         expected_file = (PROCESSING_FOLDER / 'tasktracker_content_collector' / f'expected_{course_name}.yaml')
-        difference = DeepDiff(yaml_as_dict(file), yaml_as_dict(expected_file), ignore_order=True)
+        difference = DeepDiff(parse_yaml(file), parse_yaml(expected_file), ignore_order=True)
         assert not difference
